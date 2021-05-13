3b225234652ebeee8152d0faff79fbe4f01976f9
unknown <shahrukh1494@gmail.com>
2021-05-13 16:42:27 +0530

    getStaticPaths and useRouter

---
+++ pages/[id].js
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+
+export function getStaticProps(context) {
+  const { params } = context;
+
+  return {
+    props: { userCount: Math.random() },
+    revalidate: 5,
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    fallback: true,
+    paths: [
+      { params: { id: "product-1" } },
+      { params: { id: "product-2" } },
+      { params: { id: "product-3" } },
+      { params: { id: "product-4" } },
+    ],
+  };
+}
+
+export default function Home(props) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
+  return <h1>LOADED</h1>;
+}
